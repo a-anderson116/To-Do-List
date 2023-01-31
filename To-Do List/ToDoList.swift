e4a2ac55004ee39e5485487e94dab2117c7baff8
Alex Anderson <andersonaj116@gmail.com>
2023-01-31 09:33:00 -0600

    Created add Item view

--- To-Do List/ToDoList.swift
+++ To-Do List/ToDoList.swift
@@ -6,9 +6,9 @@
 //
 
 import Foundation
-class ToDoList: ObservableObject
+class ToDoList: ObservableObject
 {
-    @Published var items = [ToDoItem(priority: "High", description: "Take out trash", dueDate: Date()),
-                            ToDoItem(priority: "Medium", description: "Pick up clothes", dueDate: Date()),
-                            ToDoItem(priority: "Low", description: "Eat a donut", dueDate: Date())]
+    @Published var items = [ToDoItem(priority: "High", description: "Take out trash", dueDate: Date()),
+                            ToDoItem(priority: "Medium", description: "Pick up clothes", dueDate:  Date()),
+                            ToDoItem(priority: "Low", description:  "Eat a donut", dueDate: Date())]
  }
